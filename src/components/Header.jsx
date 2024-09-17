d573df91b518d652f7ea2e6bb019a91afa11c390
paku <p4blo.kuhn@gmail.com>
2024-09-17 01:11:56 -0300

    extended colors in tailwind, primary color, new searchbar in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,22 +45,17 @@ const CtnRightBar = styled.div`
   }
 `
 
-const Linked = styled(Link)`
-  width: max-content;
-`
-
-export default function Header({ $Home }) {
+export default function Header() {
   return (
     <>
-      <Head $Home={$Home}>
-        <Linked title='Home' to={'/'}>
+      <Head>
+        <Link className='w-max' title='Home' to={'/'}>
           <Logo src={imgLogo} alt='' />
-        </Linked>
+        </Link>
         <CtnRightBar>
-          <SearchBar />
           <LinkButton
             className={'hide-mobile'}
-            to={'/product/monopatines'}
+            to={'/product/scooters'}
             text={'Monopatines'}
           />
           <LinkButton
@@ -71,6 +66,31 @@ export default function Header({ $Home }) {
           />
         </CtnRightBar>
       </Head>
+    </>
+  )
+}
+
+export function SearchBar() {
+  return (
+    <>
+      <div className='relative flex h-max w-max items-center justify-start'>
+        <input
+          type='text'
+          className='text-red peer rounded-xl border-[1px] 
+    border-transparent bg-gray-800 px-3 py-2 caret-primary
+    outline-none focus-visible:border-primary'
+          placeholder=' '
+        />
+
+        <p
+          className='pointer-events-none absolute -translate-y-8 px-3 
+  font-bold text-slate-300 duration-300 peer-placeholder-shown:translate-y-0
+   peer-placeholder-shown:font-normal peer-placeholder-shown:text-slate-400 
+   peer-focus:-translate-y-8 peer-focus:font-bold peer-focus:text-slate-300'
+        >
+          Email
+        </p>
+      </div>
     </>
   )
 }
