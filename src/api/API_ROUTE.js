cd22214d3f8a04ad9c95452ddf28ac708d0223cb
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-10-06 15:59:55 -0300

    changed production to development

--- src/api/API_ROUTE.js
+++ src/api/API_ROUTE.js
@@ -1,4 +1,4 @@
-const production = true
-export const API_ROUTE = production
+const developer = false
+export const API_ROUTE = developer
   ? 'http://localhost:3000/api'
   : 'https://eride-api.vercel.app/api'
