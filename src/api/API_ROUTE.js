4a7da6958f67fefd2acc5f78d5c7c46ed5a0940e
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2024-10-18 12:16:16 -0300

    added condition to API_ROUTE if is production or development

--- src/api/API_ROUTE.js
+++ src/api/API_ROUTE.js
@@ -1,1 +1,5 @@
-export const API_ROUTE = 'https://eride-api.vercel.app/api'
+const production = true
+
+export const API_ROUTE = production
+  ? 'https://eride-api.vercel.app/api'
+  : 'http://localhost:3000/api'
