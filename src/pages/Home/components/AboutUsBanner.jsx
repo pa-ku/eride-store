64e02cf7457fdd4c482d92bf8d1080fb4fc812bc
Pαblo Kuhn <40728995+pa-ku@users.noreply.github.com>
2025-03-01 02:41:35 -0300

    fixed typo in Link button

--- src/pages/Home/components/AboutUsBanner.jsx
+++ src/pages/Home/components/AboutUsBanner.jsx
@@ -3,31 +3,31 @@ import shippingIcon from '#assets/icons/shipping.webp'
 import premiumIcon from '#assets/icons/premium.webp'
 import { Link } from 'react-router-dom'
 
-export default function AboutUsBanner () {
+export default function AboutUsBanner() {
   return (
-    <div className='flex h-96 justify-center items-center bg-[#f5f5f5] p-3'>
-      <div className='flex flex-col items-center justify-center gap-10'>
-        <h2 className='subtitle'>Sobre Eride</h2>
+    <div className="flex h-96 items-center justify-center bg-[#f5f5f5] p-3">
+      <div className="flex flex-col items-center justify-center gap-10">
+        <h2 className="subtitle">Sobre Eride</h2>
 
-        <div className='justify-center flex-row flex-wrap w-full flex  gap-2'>
+        <div className="flex w-full flex-row flex-wrap justify-center gap-2">
           <Section
             src={premiumIcon}
-            alt='imagen de una estrella '
-            text='Garantizamos el máximo valor por tu inversión'
+            alt="imagen de una estrella "
+            text="Garantizamos el máximo valor por tu inversión"
           />
           <Section
             src={shippingIcon}
-            alt='icono de envio'
-            text='Tus compras cuentan con envío gratuito a todo el país'
+            alt="icono de envio"
+            text="Tus compras cuentan con envío gratuito a todo el país"
           />
           <Section
-            text='Tu privacidad es nuestra principal preocupación.'
+            text="Tu privacidad es nuestra principal preocupación."
             src={lockIcon}
-            alt='imagen de un candado'
+            alt="imagen de un candado"
           />
         </div>
 
-        <Link className='button' typeLink $altButton to='/about'>
+        <Link className="button" to="/about">
           Conocer más...
         </Link>
       </div>
@@ -35,18 +35,18 @@ export default function AboutUsBanner () {
   )
 }
 
-export function Section ({ src, alt, text }) {
+export function Section({ src, alt, text }) {
   return (
     <>
-      <div className='w-80 flex flex-col items-center justify-center'>
+      <div className="flex w-80 flex-col items-center justify-center">
         <img
           width={120}
           height={100}
-          className='pointer-events-none object-contain'
+          className="pointer-events-none object-contain"
           src={src}
           alt={alt}
         />
-        <p className='text-center'>{text}</p>
+        <p className="text-center">{text}</p>
       </div>
     </>
   )
